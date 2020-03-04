664443bcac9b0e3a1013599f12ae96b3aca88b33
bchavez <bchavez@aclaratech.com>
2020-03-04 15:08:22 -0600

    agregando hibernate branch

--- JavaHibernate/src/javahibernate/HibernateUtil.java
+++ JavaHibernate/src/javahibernate/HibernateUtil.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package pos.bl;
+package javahibernate;
 
 import org.hibernate.cfg.AnnotationConfiguration;
 import org.hibernate.SessionFactory;
@@ -12,7 +12,7 @@ import org.hibernate.SessionFactory;
  * Hibernate Utility class with a convenient method to get Session Factory
  * object.
  *
- * @author User
+ * @author billy
  */
 public class HibernateUtil {
 
